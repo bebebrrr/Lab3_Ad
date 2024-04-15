8db21dadb5a1c2434375f65dc943949177fdfe1c
bebebrrr <kubanova04@gmail.com>
2024-04-15 21:14:04 +0300

    programms

--- prog/hard.py
+++ prog/hard.py
@@ -21,26 +21,23 @@ def add(filename, number, punkt, time):
     Добавить данные о поезде.
     """
     trains = load_trains(filename)
-    trains.append(
-        {
-            "number": number,
-            "punkt": punkt,
-            "time": time,
-        }
-    )
-    # trains.append(train)
+    train = {
+        "number": number,
+        "punkt": punkt,
+        "time": time,
+    }
+    trains.append(train)
     save_trains(filename, trains)
 
 
 @commands.command("display")
 @click.argument("filename")
-@click.option("--punkts", help="Display trains data")
-def display_trains(filename, punkts):
+def display_trains(filename):
     """
     Отобразить список поездов.
     """
     trains = load_trains(filename)
-    if punkts:
+    if trains:
         # Заголовок таблицы.
         line = "+-{}-+-{}-+-{}-+-{}-+".format(
             "-" * 4, "-" * 30, "-" * 20, "-" * 17
@@ -53,7 +50,7 @@ def display_trains(filename, punkts):
         )
         print(line)
         # Вывести данные о всех поездах.
-        for idx, trains in enumerate(punkts, 1):
+        for idx, trains in enumerate(trains, 1):
             print(
                 "| {:>4} | {:<30} | {:<20} | {:>17} |".format(
                     idx,
@@ -76,12 +73,12 @@ def load_trains(filename):
         return json.load(fin)
 
 
-def save_trains(filename, number, time, punkts):
+def save_trains(filename, trains):
     """
     Сохранить все поезда в JSON.
     """
     with open(filename, "w", encoding="utf-8") as fount:
-        json.dump(punkts, time, number, fount, ensure_ascii=False, indent=4)
+        json.dump(trains, fount, ensure_ascii=False, indent=4)
 
 
 @commands.command("select")
@@ -95,7 +92,7 @@ def select(filename, filename2, timeB):
     trains = load_trains(filename)
     result = []
     for time in trains:
-        if time.get("time") >= timeB:
+        if time.get("time", 0) >= timeB:
             result.append(time)
 
     save_trains(filename2, result)
